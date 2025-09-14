e256dd261443f45cd2cce737c3cd3d5bf8c30808
Grants-UNO <grant.schaeffer@icloud.com>
2025-09-14 20:06:44 +0000

    Done with RPS

--- TempConvert.py
+++ TempConvert.py
@@ -1,17 +1,17 @@
 #TempConvert.py
-#Name:
-#Date:
-#Assignment:
+#Name: Grant Schaeffer
+#Date: 9/14/25
+#Assignment: Temperature Converter
 
 
 def main():
   #Prompt the user for a Fahrenheit temperature
   #Convert that temperature to celsius, rounding to 1 decimal percision
   #Output converted temperature.
-  tempF = 80
-
-  tempC = tempF / 2
-
+  tempF = input("What is the temperature in Fahrenheit? ")
+  tempF= float(tempF)
+  tempC = (((tempF - 32) * 5 ) / 9)
+  tempC = round(tempC,2)
   print(tempF, "is ", tempC, "degrees celsius.")
 if __name__ == '__main__':
   main()
